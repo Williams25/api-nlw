6d4a098fa00d4dacf44c9aec689dc1b9ee989b55
Williams25 <william.souza.timao@hotmail.com>
2021-04-20 15:52:44 -0300

    Instanciando a conexao e rotas para a aplicação

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,16 @@
+import createConnection from './connection'
+createConnection()
+
 import express from 'express'
 import { error, corsConfig } from './middlewares'
+import router from './routes'
 
 const app = express()
-app.use(express.json())
-corsConfig(app)
 
+app.use(express.json())
+
+corsConfig(app)
+app.use(router)
 error(app)
+
 export { app }
