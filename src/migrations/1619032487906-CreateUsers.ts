ff7cd9ff4ef543533daf40022f7aab29d3267dc2
Williams25 <william.souza.timao@hotmail.com>
2021-04-21 18:28:09 -0300

    criando migration para users

---
+++ src/migrations/1619032487906-CreateUsers.ts
@@ -0,0 +1,36 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm'
+
+export class CreateUsers1619032487906 implements MigrationInterface {
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(new Table({
+      name: 'users',
+      columns: [
+        {
+          name: 'id',
+          type: 'uuid',
+          isPrimary: true
+        },
+        {
+          name: 'email',
+          type: 'string',
+        },
+        {
+          name: 'created_at',
+          type: 'timestamp',
+          default: 'now()'
+        },
+        {
+          name: 'updated_at',
+          type: 'timestamp',
+          default: 'now()'
+        }
+      ]
+    }))
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('users')
+  }
+
+}
