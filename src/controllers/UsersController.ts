3ddfb21906ed20f29ef3506fbf28bb9e8ec0d736
Williams25 <william.souza.timao@hotmail.com>
2021-04-21 18:27:45 -0300

    criando controller para users

---
+++ src/controllers/UsersController.ts
@@ -0,0 +1,29 @@
+import { Request, Response } from 'express'
+import { UsersServices } from '../services'
+
+const service = () => new UsersServices()
+
+class UsersController {
+
+  async create(req: Request, res: Response) {
+    try {
+      const { email } = req.body
+      
+      const user = await service().create({ email })
+      return res.status(201).json(user)
+    } catch (error) {
+      return res.status(400).json({ error: error.message })
+    }
+  }
+
+  async find(req: Request, res: Response) {
+    try {
+      const users = await service().find()
+      return res.status(200).json(users)
+    } catch (error) {
+      return res.status(400).json({ error: error.message })
+    }
+  }
+}
+
+export { UsersController }
