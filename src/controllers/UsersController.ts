e8ba67b2a5985b5863f7c722fbe23a3b6daf3ac4
Williams25 <william.souza.timao@hotmail.com>
2021-04-22 19:35:55 -0300

    recuperando id

--- src/controllers/UsersController.ts
+++ src/controllers/UsersController.ts
@@ -18,7 +18,9 @@ class UsersController {
 
   async find(req: Request, res: Response) {
     try {
-      const users = await service().find()
+      const {user_id} = req.params
+      
+      const users = await service().findById(user_id)
       return res.status(200).json(users)
     } catch (error) {
       return res.status(400).json({ error: error.message })
