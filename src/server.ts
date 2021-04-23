54810b694d59ff7ca27b1449137bfc46ed3f16b1
Williams25 <william.souza.timao@hotmail.com>
2021-04-22 22:41:59 -0300

    criando servidor http

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import dotenv from 'dotenv'
-import { app } from './app'
+import app from './app'
+import './websocket/client'
 
+const { http } = app
 dotenv.config()
 
-app.listen(process.env.PORT || 3333, () => console.log(`http://localhost:${process.env.PORT || 3333}`))
+http.listen(process.env.PORT || 3333, () => console.log(`http://localhost:${process.env.PORT || 3333}`))
