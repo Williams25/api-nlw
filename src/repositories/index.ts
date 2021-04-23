3d2e59a0ea292b637a67055e684b7b359866e306
Williams25 <william.souza.timao@hotmail.com>
2021-04-22 22:41:20 -0300

    exportando mais repositorios

--- src/repositories/index.ts
+++ src/repositories/index.ts
@@ -1,3 +1,4 @@
 export { SettingsRepository } from './SettingsRepository'
 export { UsersRepositoty } from './UsersRepositoty'
 export { MessagesRepository } from './MessagesRepository'
+export { ConnectionsRepository } from './ConnectionsRepository'
