5fedcfad859aff3bcc38e26ce6c51c58e8e8fe95
Williams25 <william.souza.timao@hotmail.com>
2021-04-22 22:40:35 -0300

    criando metodos de update e finusername

--- src/controllers/SettingsController.ts
+++ src/controllers/SettingsController.ts
@@ -8,7 +8,7 @@ class SettingsController {
   async create(req: Request, res: Response) {
     try {
       const { chat, userName } = req.body
-      
+
       const settings = await service().create({ chat, userName })
       return res.status(201).json(settings)
     } catch (error) {
@@ -23,6 +23,30 @@ class SettingsController {
     } catch (error) {
       return res.status(400).json({ error: error.message })
     }
+  }
+
+  async findByUserName(req: Request, res: Response) {
+    try {
+      const { userName } = req.params
+
+      const settings = await service().findByUserName(userName)
+      return res.status(200).json(settings)
+    } catch (error) {
+      return res.status(400).json({ error: error.message })
+    }
+  }
+
+  async update(req: Request, res: Response) {
+    try {
+      const { userName } = req.params
+      const { chat } = req.body
+
+      const settings = await service().update(userName, chat)
+      return res.status(200).json(settings)
+    } catch (error) {
+      return res.status(400).json({ error: error.message })
+    }
+
   }
 }
 
