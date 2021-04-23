321aa52e7c5a2c668bbe9b104e92474372978a0f
Williams25 <william.souza.timao@hotmail.com>
2021-04-22 22:43:33 -0300

    exportando mais serviços

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,3 +1,4 @@
 export { SettingsService } from './SettingsServices'
 export { UsersServices } from './UsersServices'
 export { MessagesServices } from './MessagesServices'
+export { ConnectionsServices } from './ConnectionsServices'
