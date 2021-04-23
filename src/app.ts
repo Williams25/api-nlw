6987b2b1c2cae248c28aba5a75460f39b7a41f3d
Williams25 <william.souza.timao@hotmail.com>
2021-04-22 22:40:04 -0300

    configurando socket.io

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,32 @@
-import createConnection from './connection'
-createConnection()
-
 import express from 'express'
 import { error, corsConfig } from './middlewares'
 import { router } from './routes'
 
+import createConnection from './connection'
+createConnection()
+
+import { createServer } from 'http'
+import { Server, Socket } from 'socket.io'
+import path from 'path'
+
 const app = express()
+
+app.use(express.static(path.join(__dirname, '..', 'public')))
+app.set('views', path.join(__dirname, '..', 'public'))
+app.engine('html', require('ejs').renderFile)
+app.set('view engine', 'html')
+
+app.get('/pages/client', (req, res) => {
+  return res.render('html/client.html')
+})
+
+
+const http = createServer(app)
+const io = new Server(http)
+
+io.on('connection', (socket: Socket) => {
+  console.log('connection ', socket.id)
+})
 
 app.use(express.json())
 
@@ -13,4 +34,4 @@ corsConfig(app)
 app.use(router)
 error(app)
 
-export { app }
+export default { http, io }
